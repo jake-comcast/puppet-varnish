fc342879a00bed0b1c7d477fd06d382eee84919a
Carlos Velazquez <carlos_velazquez@cable.comcast.com>
2016-07-12 12:31:03 -0400

    add varnish_timeout_idle

--- manifests/init.pp
+++ manifests/init.pp
@@ -60,6 +60,7 @@ class varnish (
   $varnish_max_threads          = '500',
   $varnish_thread_timeout       = '300',
   $varnish_thread_add_delay     = '20',
+  $varnish_timeout_idle         = '75',
   $varnish_storage_size         = '1G',
   $varnish_secret_file          = '/etc/varnish/secret',
   $varnish_storage_file         = '/var/lib/varnish-storage/varnish_storage.bin',
